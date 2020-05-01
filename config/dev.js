224c504e8c0ffa8298d8d3049efc67b9421f824b
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-05-01 13:08:42 +0300

    OAuth finish

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientSecret: "d7ql4Ko-jQW1xfRZvprGA11P",
+  googleClientID:
+    "135038317826-6sodntgt5p87kq4fp5q11dngmv2gd2ch.apps.googleusercontent.com",
+  mongoURI:
+    "mongodb+srv://emaily:qMmkoYGwEn8jA9j0@cluster0-pipax.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "badoinkadong",
+};
