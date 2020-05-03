ba5f74f0a680c1b84647a23b56aaded26de068f1
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-05-03 16:38:36 +0300

    dirname

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   //express will serve up index.html file if does not recognize path
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
